bbde4e7f4568fad819793864f4de39e12fb8e33d
yixiyang <yixiy0916@gmail.com>
2014-08-17 05:23:15 -0700

    update
    try to upload network data (two files, one relations and the other for
    vertex), add Help tab panel and add html of information about statnet
    and network, run through parameters of network packages and debug

---
+++ week 5(8.5 ~8.12)/ui.R
@@ -0,0 +1,228 @@
+# Week 5
+# Yixi Yang
+# tasks: upload network data (two files, one for relations and the other for vertex), add Help tabpanel and 
+# add html of information about statnet and network, run through parameters of network and debug
+
+# TODO: Add comment
+# 
+# Author: kirk
+###############################################################################
+library(shiny)
+library(network)
+library(ergm)
+source("chooser.R")
+source("functions.R")
+
+# write uiOutput for Modifying parameter value. 
+UI_G1 <- function(group=NULL){
+ tmp <- lapply(1:12,function(ct) {eval(parse(text=paste("uiOutput('para_",group,".",ct,"')",sep="",collapse="")))})
+ tmp
+}
+
+# function to generate dynamic ui for Type2
+#UI_G2 <- function(){
+# tmp <- lapply(1:12,function(ct) {eval(parse(text=paste("uiOutput('paraT2.",ct,"')",sep="",collapse="")))})
+# tmp
+#}
+
+customTextInput<-function (inputId, label, value="",...) {
+ tagList(tags$label(label, `for` = inputId), tags$input(id = inputId,
+     type="text",
+     value=value,...))
+}
+
+
+# This function generates the client-side HTML for a helper button
+helperButton <- function(id) {
+  tagList(
+    tags$button(id=id, type="button", class="helper-btn",
+                tags$img(src= "200px-Icon-round-Question_mark.svg.png",
+                         height = 20, width = 20))
+  )
+}
+
+
+shinyUI(fluidPage(
+    titlePanel("network app"),
+    splitLayout( 
+      wellPanel(width=12, 
+        tabsetPanel(
+          
+          
+     #     bootstrapPage(title='statnet - ergm app',
+      #                  navbarPage(title=div(a(span('statnet  ', style='font-family:Courier'),
+       #                                        href = 'https://statnet.csde.washington.edu/trac',
+        #                                       target = '_blank'),HTML('&nbsp;&nbsp;'), 'ergm app'),
+                 
+          tabPanel('Data',
+            fluidRow(
+              column(12,
+                wellPanel(
+                  
+                  h3('Upload own files'),
+                  fileInput('file1', 'Choose relation file to upload',
+                            accept = c(
+                              'text/csv',
+                              'text/comma-separated-values',
+                              'text/tab-separated-values',
+                              'text/plain',
+                              '.csv',
+                              '.tsv'
+                            )
+                  ),
+                  fileInput('file2', 'Choose Vertex file to upload',
+                            accept = c(
+                              'text/csv',
+                              'text/comma-separated-values',
+                              'text/tab-separated-values',
+                              'text/plain',
+                              '.csv',
+                              '.tsv'
+                            )
+                  ),
+                  tags$hr(),
+                  
+                  
+                  h3('Choose a dataset'),
+                  selectInput('dataset',
+                    label = 'Sample datasets',
+                    c(Choose = '', 'ecoli1', 'ecoli2', 'faux.mesa.high',
+                      'fauxhigh', 'flobusiness','flomarriage',
+                      'kapferer','kapferer2','samplike'),
+                    selectize = FALSE),
+                  br(),
+                  actionButton('goButton', 'Run')))
+            )),
+          tabPanel('Generic',
+            fluidRow(
+              column(12,
+                wellPanel(
+                  chooserInput("mychooser_generic", "Available arguments", "Selected 
+                      metric",generic.arg.vec(), c(), size = 10, multiple = TRUE
+                  )),
+                h3("Parameter Value"),
+#																wellPanel(
+                column(5,
+                  UI_G1("generic")),
+                column(1),
+                column(6,
+                  verbatimTextOutput("exp_generic"))# written in function.R
+              )
+            )),
+          tabPanel('Layout',
+            fluidRow(
+              column(12,
+                wellPanel(
+                  chooserInput("mychooser_layout", "Available arguments", "Selected 
+                      metric",layout.arg.vec(), c(), size = 10, multiple = TRUE
+                  )),
+                h3("Parameter Value"),
+#																wellPanel(
+                column(5,
+                  UI_G1("layout")),
+                column(1),
+                column(6,
+                  verbatimTextOutput("exp_layout"))# written in function.R
+              )
+            )),									
+          tabPanel('Vertex',
+            fluidRow(
+              column(12,
+                wellPanel(
+                  chooserInput("mychooser_vertex", "Available arguments", "Selected 
+                      metric",vertex.arg.vec(), c(), size = 10, multiple = TRUE
+                  )),
+                h3("Parameter Value"),
+#																wellPanel(
+                column(5,
+                  UI_G1("vertex")),
+                column(1),
+                column(6,
+                  verbatimTextOutput("exp_vertex"))# written in function.R
+              )
+            )),
+          tabPanel('Edge',
+            fluidRow(
+              column(12,
+                wellPanel(
+                  chooserInput("mychooser_edge", "Available arguments", "Selected 
+                      metric",edge.arg.vec(), c(), size = 10, multiple = TRUE
+                  )),
+                h3("Parameter Value"),
+#																wellPanel(
+                column(5,
+                  UI_G1("edge")),
+                column(1),
+                column(6,
+                  verbatimTextOutput("exp_edge"))# written in function.R
+              )
+            )),
+
+
+          # tabPanel('Help',htmlOutput("help",inline=FALSE))
+
+tabPanel('Help',htmlOutput("help1",inline=FALSE),hr(),htmlOutput("help2",inline=FALSE))
+
+#          tabPanel('Graphical Parameter Type 1',
+#            fluidRow(
+#              column(4,
+#                wellPanel(
+#                  actionButton('Type1',"Click to Use Type1"),
+#                  chooserInput("mychooser", "Available arguments", "Selected 
+#                      metric",network.arg.vec(), c(), size = 10, multiple = TRUE
+#                  )),
+#                wellPanel(
+#                  h3("Parameter Value"),
+#                  UI_G1() # written in function.R
+#                )
+#              ))),
+#          tabPanel('Graphical Parameter Type 2',
+#            fluidRow(
+#              column(3, wellPanel(
+#                  actionButton('Type2',"Click to Use Type2"),
+#                  h4("Choose arguments"),
+#                  checkboxGroupInput("net.ele",
+#                    "Network Elements:",
+#                    choices = list(
+#                      "edge" = "edge",
+#                      "vertex" = "vertex")),
+#                  checkboxGroupInput("net.eff",
+#                    "Network Effect:",
+#                    choices = list(
+#                      "cex" = "cex",
+#                      "col" = "col"))
+#                ),
+#                
+#                wellPanel(
+#                  h3("Parameter Value"),
+#                  UI_G2() # written in function.R
+#                )        
+#              
+#              )
+#            )),
+        )),
+      
+      wellPanel(   
+        h4('Console Message'),
+        verbatimTextOutput('console'),	  
+        h4('Network Plot'),
+        plotOutput('nwplot'),
+        h4('Network Summary'),
+        uiOutput("attrUI"),
+        uiOutput("servertest"),
+        h4('Diagnose Message'),
+        verbatimTextOutput('diag'),
+        downloadLink('downloadData', 'Download'))
+    ),
+    
+    fluidRow(
+      column(1, img(src = 'csdelogo_crop.png', height = 50, width = 50)),
+      column(2, h6('Center for Studies in Demography and Ecology'))
+    )
+  )
+)
+
+
+
+
+
